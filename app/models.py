41d9e8fa817df9ca6840bd1914ce622a128696fb
plongbi <150523131+plongbi@users.noreply.github.com>
2025-05-06 22:35:59 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -40,10 +40,8 @@ class Product(models.Model):
     
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
-    date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
-    transaction_id = models.CharField(max_length=200, null=True)
-
+    
     def __str__(self):
         return str(self.id)
 
@@ -64,11 +62,11 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
-    transaction_id = models.CharField(max_length=200, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
 
     @property
     def get_total(self):
         if self.product:
             return self.product.price * self.quantity
         return 0
+
+
